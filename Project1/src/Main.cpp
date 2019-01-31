3b66deeab8ec8f5a289fe78c4131ad460c8b682e
ellinger89 <ellinger1989@gmail.com>
2019-01-31 14:49:05 +0100

    first (local) commit ont home laptop

--- Project1/src/Main.cpp
+++ Project1/src/Main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+# include <string>
 using namespace std;
+
+template<typename T>
+void print(T x) {
+	cout << x << endl;
+}
+
+template<typename T, int size>
+class myArray{
+private:
+		T m_array[size];
+public: 
+		int getSize() {
+			return(size);
+	}
+};
 
 
 int main(){
-	cout << "xxx" << endl;
-	cout << "i've been added online and synced with the local verson via git fetch upstream" << endl;
+
+	const int maxage = 90;
+
+	int*a = new int;
+	*a = 5;
+	a = (int*)&maxage;
+
+	print(*a);
+
 	cin.get();
 }
