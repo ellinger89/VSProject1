670268975ccbd5742b4cfca9c523b3f99dfef6e4
Andreas Ellinger <ellinger1989@gmail.com>
2019-02-01 17:02:24 +0100

    changes on explore-tablett

--- Project1/src/Main.cpp
+++ Project1/src/Main.cpp
@@ -2,6 +2,7 @@
 # include <string>
 using namespace std;
 
+/*
 template<typename T>
 void print(T x) {
 	cout << x << endl;
@@ -16,17 +17,11 @@ public:
 			return(size);
 	}
 };
-
+*/
 
 int main(){
 
-	const int maxage = 90;
-
-	int*a = new int;
-	*a = 5;
-	a = (int*)&maxage;
-
-	print(*a);
+	cout << "hello CMake!" << endl;
 
 	cin.get();
 }
---
+++ Project1/src/backups.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+# include <string>
+using namespace std;
+
+/*
+ 
+template<typename T>
+void print(T x) {
+	cout << x << endl;
+}
+
+template<typename T, int size>
+class myArray {
+private:
+	T m_array[size];
+public:
+	int getSize() {
+		return(size);
+	}
+};
+
+
+int main() {
+
+	const int maxage = 90;
+
+	int*a = new int;
+	*a = 5;
+	a = (int*)&maxage;
+
+	print(*a);
+
+	cin.get();
+}
+
+
+
+*/
