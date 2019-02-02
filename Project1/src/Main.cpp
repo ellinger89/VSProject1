f9e9d89a76bcc0804cd68e1539382288bc47060f
ellinger89 <ellinger1989@gmail.com>
2019-02-02 12:59:26 +0100

    main.cpp changed completly, is now example of 'mutable'-usecase

--- Project1/src/Main.cpp
+++ Project1/src/Main.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
 # include <string>
-using namespace std;
 
-/*
+
 template<typename T>
 void print(T x) {
-	cout << x << endl;
+	std::cout << x << std::endl;
 }
 
-template<typename T, int size>
-class myArray{
+class Entity{
 private:
-		T m_array[size];
-public: 
-		int getSize() {
-			return(size);
+	// since this variable will be changed from within a method that 
+	// promised not to change anything (~ method() const {}), the
+	// variable must be explicitly marked as mutable
+	mutable std::string mystring;
+public:
+	// for a method to be called from a const object,
+	// the method must "promise" that it is not going to change anything
+	std::string& getX() const		
+	{
+		return mystring;
+	}
+	void setX(std::string someString) const
+	{
+		mystring = someString;
 	}
 };
-*/
+
 
 int main(){
 
-	// will this change show up in the next pull?
-	std::cout << "some online changes" std::endl;
-	
-	// end of testchange
-	
-	cout << "hello CMake!" << endl;
+	const Entity obj;		// somehow you want an object that can not be changed
+	obj.setX("hubuduboo");
 
+	// so now, allthough all the methods involved promised that they
+	// will not change any members of the Entity-Class, those methods
+	// can still set and return the member "mystring", because it was
+	// explicitly marked as mutable
+	std::string a;
+	a = obj.getX();
+	print(a);
 
-	cin.get();
+	std::cin.get();
 }
